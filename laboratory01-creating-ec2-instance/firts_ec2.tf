b0d7dc757620142d3d32aa58228d3389e45242e0
sirfacu <64324089+sirfacu@users.noreply.github.com>
2022-08-24 18:49:01 -0500

    Update firts_ec2.tf

--- laboratory01-creating-ec2-instance/firts_ec2.tf
+++ laboratory01-creating-ec2-instance/firts_ec2.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIATQHYAAGUZAVTXOXX"
-  secret_key = "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"
 }
 
 resource "aws_instance" "my_ec2" {
