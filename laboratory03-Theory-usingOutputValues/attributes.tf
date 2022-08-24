e8690b318a5a66338cf434267ac88e6fa8134b36
sirfacu <64324089+sirfacu@users.noreply.github.com>
2022-08-24 18:49:52 -0500

    Update attributes.tf

--- laboratory03-Theory-usingOutputValues/attributes.tf
+++ laboratory03-Theory-usingOutputValues/attributes.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIATQHYAAGUZAVTXOXX"
-  secret_key = "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"
+  profile    = "sirfacu"
 }
 
 resource "aws_eip" "lb" {
